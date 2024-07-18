ab8bbbefd29a9e4a288f624f7ede913c222e7d04
Ram Ashish <aram70802@gmail.com>
2024-07-18 22:34:13 +0530

    switch case added

--- second.js
+++ second.js
@@ -3,3 +3,23 @@ let objectNew = {
     collge: "object",
     compNm: "knovator"
 }
+
+/**
+ * Hii how are you
+ */
+const findDateUsingNumber = (dayName) => {
+    switch (dayName) {
+        case 1:
+            console.log("first Data");
+            break;
+        case 2:
+            console.log("second Date");
+            break;
+        case 3:
+            console.log("third Date");
+            break;
+        default:
+            console.log("wrong date")
+    }
+}
+findDateUsingNumber();
