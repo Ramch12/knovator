f7913f74eda3bb9189cb8cc30b5e0a3cd7c0590c
Ram Ashish <aram70802@gmail.com>
2024-07-18 22:26:40 +0530

    second commit

--- second.js
+++ second.js
@@ -3,3 +3,27 @@ let objectNew = {
     collge: "object",
     compNm: "knovator"
 }
+
+
+/**
+ * Hii i am doing this to make a confict so that i can resolve it
+ * @param {new object}
+ */
+
+function calculateSumforcomplexOperation() {
+    let sum = 0;
+    let newObjectTosum = {
+        first: 1,
+        second: 2,
+        third: 3,
+        fourth: 4,
+        fifth: 5,
+        sixth: 6
+    };
+    Object.keys(newObjectTosum).forEach((item) => {
+        sum += newObjectTosum[item]
+    });
+    console.log(`sum is ${sum}`);
+}
+calculateSumforcomplexOperation();
+
